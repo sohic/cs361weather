990cdb2c7fc867d91374d1cfd4d19bc61af4048b
Chandan Sohi <91230351+sohic@users.noreply.github.com>
2024-05-31 22:27:51 -0700

    updated app.py

--- app.py
+++ app.py
@@ -3,32 +3,49 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-def get_weather(zipcode):
+def get_coord(zipcode):
     api_key = '03d39d0a4844727881ad16a48828f5f0'
-    url = f'http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid={api_key}&units=metric'
+    url = f'https://api.openweathermap.org/geo/1.0/zip?zip={zipcode}&appid={api_key}'
     response = requests.get(url)
     data = response.json()
     return data
 
+def get_current(zipcode, lat, lon):
+    api_key = '03d39d0a4844727881ad16a48828f5f0'
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,daily,alerts&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        current = data["current"]
+        response_data = {
+            'temperature': current["temp"],
+            'description': current["weather"][0]["description"],
+            'timezone': data["timezone"],
+            'zipcode': zipcode
+        }
+        
+    else:
+        response_data = {
+        'error': "unknown error"
+    }
+    return response_data
+
 @app.route('/<zipcode>', methods=['GET'])
 def weather(zipcode):
 
-    # Get the weather data for the provided zip code
-    weather_data = get_weather(zipcode)
+    coord_data = get_coord(zipcode)
 
-    if 'cod' in weather_data and weather_data['cod'] == '404':
-        return jsonify({'error': 'Weather data not found'}), 404
+    if 'cod' in coord_data and coord_data['cod'] == '400':
+        return jsonify({'error': 'Invalid Zip Code'}), 400
+    
+    if 'cod' in coord_data and coord_data['cod'] == '404':
+        return jsonify({'error': 'Coordinates not found'}), 404
 
     # Extract relevant weather information
-    temperature = weather_data['main']['temp']
-    description = weather_data['weather'][0]['description']
-
-    # Prepare the response
-    response_data = {
-        'zip_code': zipcode,
-        'temperature': temperature,
-        'description': description
-    }
+    lat = coord_data['lat']
+    lon = coord_data['lon']
+    response_data = get_current(zipcode, lat, lon)
+    
 
     # Return the weather data as JSON
     return jsonify(response_data)
